667a49fde935e1ba2d9c2d2a67480b31854bd977
Dominic1234 <dhruv.sharma.017@gmail.com>
2017-10-10 01:56:44 +0530

    Added MyApp

---
+++ app/src/main/java/com/sharma/dhruv/autotaxi/MyApp.java
@@ -0,0 +1,13 @@
+package com.sharma.dhruv.autotaxi;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class MyApp extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
